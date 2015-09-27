295cc61b38e9bcacdf81e32ce5ce7fba77790be7
Knut Aldrin Wikström <knutaldrin@gmail.com>
2015-09-27 21:31:42 +0200

    Refactor blinkyphase

--- generator/src/java/darkness/generator/scripts/uka15/IntroJanosa.java
+++ generator/src/java/darkness/generator/scripts/uka15/IntroJanosa.java
@@ -3,6 +3,7 @@ package darkness.generator.scripts.uka15;
 import darkness.generator.api.BulbGroup;
 import darkness.generator.api.BulbManager;
 import darkness.generator.api.BulbRGB;
+import darkness.generator.api.effects.BlinkyPhase;
 import darkness.generator.api.effects.EffectBase;
 
 import java.awt.*;
@@ -125,44 +126,4 @@ public class IntroJanosa extends BaseScript {
             skip(4);
         }
     }
-
-	private class BlinkyPhase extends EffectBase {
-		private boolean cancelled = false;
-		// Pseudorandom with fixed seed. Can be changed to get a different, but similar effect
-		private Random random = new Random(1337);
-		@Override
-		public void run() {
-            LinkedList<BulbRGB> freeBulbs = new LinkedList<BulbRGB>(BulbManager.getInstance().getAllBulbs());
-			while (!(cancelled || freeBulbs.isEmpty()))
-			{
-                int bulbIndex = random.nextInt(freeBulbs.size());
-                BulbRGB bulb = freeBulbs.get(bulbIndex);
-                if(bulb.getRed() != 0 || bulb.getGreen() != 0 || bulb.getBlue() != 0)
-                {
-                    // This bulb is in use. Lets find a new one
-                    freeBulbs.remove(bulbIndex);
-                    continue;
-                }
-                setHSB(bulb, random.nextFloat(), 0.6f, 0.7f);
-                Color color = bulb.getColor();
-                next();
-                // The color has not changed by another effect. Turn it off
-                if(bulb.getColor().equals(color))
-                {
-                    set(bulb, 0, 0, 0);
-                }
-			}
-		}
-
-		@Override
-		public String toString() {
-			return "BlinkyEffect";
-		}
-
-		@Override
-		public void cancel() {
-			cancelled = true;
-		}
-
-	}
 }
