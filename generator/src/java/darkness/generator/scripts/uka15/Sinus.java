44abfa854e17f2319cd094ffacaaec2064218265
Åsmund Eldhuset <aasmund.eldhuset@gmail.com>
2015-09-30 21:00:46 +0200

    Updated Sinus sequence.

--- generator/src/java/darkness/generator/scripts/uka15/Sinus.java
+++ generator/src/java/darkness/generator/scripts/uka15/Sinus.java
@@ -8,9 +8,9 @@ public class Sinus extends BaseScript {
 		super.run();
 		final double middleBottom = 0.5;
 		final double blurRadius = 0.2f;
-		final double frequencyMultiplier = 1.01;
-		final int iterations = 600;
-		double frequency = 0.1;
+		final double frequencyMultiplier = 1.007;
+		final int iterations = 400;
+		double frequency = 0.2;
 		double t = 0;
 		for (int i = 0; i < iterations; t += 0.03, i++, frequency *= frequencyMultiplier) {
 			for (BulbRGB bulb : allBulbs) {
@@ -18,9 +18,21 @@ public class Sinus extends BaseScript {
 				final float y = bulb.getPosition()[1];
 				final double bottom = (1 - Math.abs(((LEFT + RIGHT) / 2 - x) / (WIDTH / 2))) * middleBottom;
 				final double sin = bottom + (Math.sin((x - LEFT + t) * frequency) + 1) / 2 * (LETTER_HEIGHT - bottom);
-				setCoerced(bulb, (sin - (y - blurRadius)) / (blurRadius * 2) * 255, 0, 0);
+				final double red = (sin - (y - blurRadius)) / (blurRadius * 2) * 255;
+				if (i == 0) {
+					rgbFade(bulb, red < 0 ? 0 : (red > 255 ? 255 : (int) Math.round(red)), 0, 0, 20);
+				} else {
+					setCoerced(bulb, red, 0, 0);
+				}
 			}
-			next();
+			if (i == 0) {
+				skip(20);
+			} else {
+				next();
+			}
+		}
+		for (BulbRGB bulb : allBulbs) {
+			rgbFade(bulb, 0, 0, 0, 20);
 		}
 	}
 }
