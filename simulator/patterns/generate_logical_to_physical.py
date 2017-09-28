6bda9d250a46a71a2f9b46906343a70f05413257
Åsmund Eldhuset <aasmund.eldhuset@gmail.com>
2017-09-28 22:30:08 +0200

    Box file and mapping from logical channels to physical channels.

--- simulator/patterns/generate_logical_to_physical.py
+++ simulator/patterns/generate_logical_to_physical.py
@@ -1,14 +1,17 @@
-from sys import argv
+from sys import argv, exit
 
 bulbs = {}
 patternFile = argv[1]
 boxFile = argv[2]
 for line in open(patternFile):
 	items = line.split()
+	if not items or not items[0].isdigit():
+		continue
 	bulb = int(items[0])
 	rgb = [int(x) for x in items[4:7]]
 	bulbs[bulb] = rgb
 
+seenBoxItems = set()
 for line in open(boxFile):
 	items = line.split()
 	if not items or items[1] == '-':
@@ -16,6 +19,10 @@ for line in open(boxFile):
 	boxItems = items[0].split('.')
 	box = int(boxItems[0])
 	boxLed = int(boxItems[1])
+	boxItem = (box, boxLed)
+	if boxItem in seenBoxItems:
+		print "Duplicate box:", boxItem
+		exit(1)
 	bulb = int(items[1])
 	rgb = bulbs[bulb]
 	for i in range(len(rgb)):
