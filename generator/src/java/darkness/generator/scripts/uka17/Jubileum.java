cc0e17c453055d637cf7f624539a3d4a11f7040d
Jens Abraham <jensab@samfundet.no>
2017-09-30 16:18:11 +0200

    tuning Jubileum to be a bit softer by fading

--- generator/src/java/darkness/generator/scripts/uka17/Jubileum.java
+++ generator/src/java/darkness/generator/scripts/uka17/Jubileum.java
@@ -3,45 +3,43 @@ package darkness.generator.scripts.uka17;
 import darkness.generator.api.BulbGroup;
 import darkness.generator.api.BulbRGB;
 
+import java.awt.*;
+
 public class Jubileum extends BaseScript {
     @Override
     public void run() {
         super.run();
-        for (BulbGroup digit : digits) {
-	    for (BulbRGB bulb : digit) {
-		set(bulb, 255, 255, 255);
-                skip(2);
-	    }
-	}
-	// hardcoded fill last element
-	set(bulb(95), 255, 255, 255);
-	skip(2);
-	set(bulb(85), 255, 255, 255);
-	set(bulb(90), 255, 255, 255);
-	skip(2);
-	set(bulb(86), 255, 255, 255);
-	set(bulb(91), 255, 255, 255);
-	skip(2);
-	set(bulb(87), 255, 255, 255);
-	set(bulb(92), 255, 255, 255);
-	skip(2);
-	set(bulb(88), 255, 255, 255);
-	set(bulb(93), 255, 255, 255);
-	skip(2);
-	set(bulb(89), 255, 255, 255);
-	set(bulb(94), 255, 255, 255);
-	skip(2);
 
-	// flash last \0/
-	skip(3);
-	int repeats = 5;
-	while (repeats > 0) {
-	    set(I, 0, 0, 0);
-	    skip(5);
-	    set(I, 255, 255, 255);
-	    skip(10);
-	    repeats -= 1;
-	}
-	skip(10);
+        Color c = new Color(255, 255, 255);
+        int t = 3;
+        int fade_ext = 17;
+
+        for (BulbGroup digit : digits) {
+	        for (BulbRGB bulb : digit) {
+		        rgbFade(bulb, c, t + fade_ext);
+                skip(t);
+	        }
+            skip(fade_ext);
+	    }
+	    // hardcoded fill last element
+	    rgbFade(bulb(95), c, t + fade_ext);
+	    skip(t);
+        for (int i = 0; i < 5; i++) {
+	        rgbFade(bulb(85+i), c, t + fade_ext);
+	        rgbFade(bulb(90+i), c, t + fade_ext);
+	        skip(t);
+        }
+
+	    // flash last \0/
+	    skip(fade_ext);
+	    int repeats = 3;
+	    while (repeats > 0) {
+	        set(I, 0, 0, 0);
+	        skip(t);
+	        set(I, c);
+	        skip(3*t);
+	        repeats -= 1;
+	    }
+	    skip(3*t);
     }
 }
