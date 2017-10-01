2649f8a3eb3824a6f2e3846176b89d37bf7522ac
crispocolus <kristoffer.selbekk.hille@gmail.com>
2017-10-01 22:16:07 +0200

    kristosh la til en testsekvens takk og hade

---
+++ generator/src/java/darkness/generator/scripts/uka17/TestSekvens.java
@@ -0,0 +1,52 @@
+package darkness.generator.scripts.uka17;
+
+import darkness.generator.api.BulbGroup;
+import darkness.generator.api.BulbRGB;
+
+import java.awt.Color; 
+
+public class TestSekvens extends BaseScript {
+	@Override
+	public void run() {
+		super.run();
+		
+		//BulbGroup[] bunnGroups = {
+		//	group(0, 7, 15, 16, 19, 33, 38, 45, 50, 49, 68, 78, 95), 
+		//	group(1, 8, 17, 20, 25, 32, 39, 46, 51, 63, 69, 77, 79, 85, 90), 
+		//	group(2, 9, 18, 21, 26, 31, 37, 47, 52, 55, 62, 67, 76, 82, 80, 86, 91), 
+		//	group(3, 10, 30, 36, 40, 48, 53, 54, 61, 66, 70, 83, 87, 92),
+		//	group(4, 11, 35, 41, 56, 60, 65, 71, 66, 72, 75, 84, 81, 88, 93),
+		//	group(6, 34, 42, 73, 89, 94), 
+		//	group(5, 43),
+		//}; 
+		
+		Color foo = new Color(255, 255, 56);
+		foo.getRed();
+		foo.getBlue();
+		foo.getGreen();
+		
+		BulbGroup previousLetter = null;
+		int hei = 0; 
+	
+			for (BulbGroup letter: letters) {		
+				for (int i = 0; i < 10; i++) { 
+					set(letter, foo);
+					skip (1); 
+					set (letter, 0, 0, 0);
+					skip(1);	
+					if (previousLetter != null) {	
+						set(previousLetter, 255, 255, 255);		
+					}
+						previousLetter = letter; 	
+				}
+				
+								
+			//col1 = col1+3; 
+			//col2 = col2+3; 
+			//col3 = col3+3; 
+			//if (col1 > 255 | col2 > 255 | col3 > 255) {
+			//	break;
+			//}
+		}
+	}
+}
