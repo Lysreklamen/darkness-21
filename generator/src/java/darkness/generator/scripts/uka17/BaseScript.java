f7e740984e70fde82223ae88593359c761127dab
Morten Liland <morten.liland@gmail.com>
2017-10-01 18:47:30 +0200

    added allBulbsBG to BaseScript.java and added YellowBang.java

--- generator/src/java/darkness/generator/scripts/uka17/BaseScript.java
+++ generator/src/java/darkness/generator/scripts/uka17/BaseScript.java
@@ -23,6 +23,8 @@ public class BaseScript extends ScriptBase {
     protected BulbGroup digitC;
     protected BulbGroup[] digits;
     protected BulbGroup[] columns;
+    protected BulbGroup[] allBulbsBG;
+
     protected BulbRGB[] allBulbs; // ...except for the counter
 
     protected BulbGroup[] counter;
@@ -61,6 +63,18 @@ public class BaseScript extends ScriptBase {
         digitB = group(30, 31, 32, 33, 38, 39, 40, 41, 42, 43);
         digitC = group(60, 61, 62, 63, 68, 69, 70, 71, 72, 73);
         digits = new BulbGroup[]{digitA, digitB, digitC};
+
+        allBulbsBG = new BulbGroup[]{
+            group(0,1,2,3,4,5,6,7,8,9,10,11),
+            group(15,16,17,18,19,20,21),
+            group(25,26),
+            group(30,31,32,33,34,35,36,37,38,39,40,41,42,43),
+            group(45,46,47,48,49,50,51,52,53,54),
+            group(55,56),
+            group(60, 61,62,63,64,65,66,67,68,69,70, 71,72,73),
+            group(75,76,77,78,79,80,81,82,83,84),
+            group(85,86,87,88,89,90,91,92,93,94,95),
+        };
 
         columns = new BulbGroup[]{
             group(6),
