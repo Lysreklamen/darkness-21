12ca83cdf2de1bd8ba27e67ee19e4834f4d240d0
Knut Aldrin <buz@samfundet.no>
2017-10-03 16:22:59 +0200

    Make Vegas longer and prettier

--- generator/src/java/darkness/generator/scripts/uka17/Vegas.java
+++ generator/src/java/darkness/generator/scripts/uka17/Vegas.java
@@ -19,51 +19,84 @@ public class Vegas extends BaseScript {
         // Ugly code ahead //
         ////// CAUTION //////
 
-        BulbGroup groupA, groupB;
-        LinkedList<BulbRGB> a = new LinkedList<>();
-        LinkedList<BulbRGB> b = new LinkedList<>();
-        boolean flipflop = true;
+        // Number of partitions
+        int numGroups = 3;
 
-        // TODO: Probably a smooth functional way to do this
-        // Setup bulb groups for vegas sign digits
-        for (BulbGroup digit: digits) {
-            for (BulbRGB bulb: digit) {
-                if (flipflop)
-                    a.add(bulb);
-                else
-                    b.add(bulb);
+        LinkedList<BulbRGB>[] arrays = new LinkedList[numGroups];
 
-                flipflop = !flipflop;
+        for(int i = 0; i < numGroups; ++i)
+            arrays[i] = new LinkedList<>();
+
+        BulbGroup[] groups = new BulbGroup[numGroups];
+
+        // Don't pollute
+        {
+            int count = 0;
+
+            // TODO: Probably a smooth functional way to do this
+            // Setup bulb groups for vegas sign digits
+            for (BulbGroup digit : digits) {
+                for (BulbRGB bulb : digit) {
+                    arrays[count].add(bulb);
+                    ++count;
+
+                    if (count >= numGroups)
+                        count = 0;
+                }
             }
         }
 
-        groupA = new BulbGroup(a.stream().toArray(BulbRGB[]::new));
-        groupB = new BulbGroup(b.stream().toArray(BulbRGB[]::new));
+        for (int i = 0; i < numGroups; ++i) {
+            groups[i] = new BulbGroup(arrays[i].stream().toArray(BulbRGB[]::new));
+        }
 
         // Feel free to open your eyes now
-        // TODO: The colours used are for demonstration purposes, and should probably be changed by someone more visual than me.
+
+        // I wish java had structs
+        final Color COL_startBg = new Color(88, 24, 69);
+        final Color COL_endBg = new Color(39, 41, 109);
+        final Color COL_fan = new Color(109, 128, 55);
+        final Color COL_bulbOn = Color.WHITE;
+        final Color COL_bulbOff = null; // Relinquish
 
         // Initial sign colour
-        for (BulbRGB bulb: allBulbsRGB)
-            set(bulb, new Color(153, 0, 0));
+        rgbFade(mergedAllBulbs, COL_startBg, 10);
+        skip(10);
 
         // Vegas casino neon sign effect
-        Cycle cycle = new Cycle(Color.WHITE, Color.DARK_GRAY, 10, groupA, groupB);
+        Cycle cycle = new Cycle(COL_bulbOn, COL_bulbOff, 3, groups);
         effect(cycle);
 
         // Scrolling fan, nice background indeed
-        FanScroll fan = new FanScroll(allBulbsRGB, 60, new Color(64, 128, 0), true, false, 1.0);
+        FanScroll fan = new FanScroll(allBulbsRGB, 60, COL_fan, true, false, 2.0);
         effect(fan);
 
-        // Fade background to another colour while all the other shit is running
-        for (BulbRGB bulb: allBulbsRGB)
-            rgbFade(bulb, new Color(139, 0, 139), 120);
+        for(int i = 0; i < 2; ++i) {
 
-        // Yep yep yep
-        skip(120);
+            // Fade background to another colour while all the other shit is running
+            for (BulbRGB bulb : allBulbsRGB)
+                rgbFade(bulb, COL_endBg, 120);
+
+            // Yep yep yep
+            skip(120);
+
+            // Fade back
+            for (BulbRGB bulb : allBulbsRGB)
+                rgbFade(bulb, COL_startBg, 120);
+
+            // Herp derp
+            skip(120);
+        }
 
         // Cleanup
         fan.cancel();
         cycle.cancel();
+
+        // Fade out
+        for (BulbRGB bulb: mergedAllBulbs)
+            rgbFade(bulb, Color.BLACK, 10);
+
+        // Wait for fade
+        skip(10);
     }
 }
