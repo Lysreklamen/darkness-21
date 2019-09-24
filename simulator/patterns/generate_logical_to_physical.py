fa29b2dce3fef10fc659237f60595351375023d9
Aasmund Eldhuset <aasmund.eldhuset@gmail.com>
2019-09-24 21:41:44 +0200

    Box file and logical to physical mapping file.

--- simulator/patterns/generate_logical_to_physical.py
+++ simulator/patterns/generate_logical_to_physical.py
@@ -1,3 +1,12 @@
+# Given a pattern file (which maps each bulb ID to a unique logical DMX channel)
+# and a box file (which maps each DMX box and LED to a unique bulb ID), this script
+# generates a systematic mapping from each logical DMX channel to a physical DMX channel.
+# Given a 1-indexed `box`, a 1-indexed `led` within that box (between 1 and 6),
+# and a color that is either red (0), green (1), or blue (2), the physical
+# channel will be assigned according to this formula:
+# (box - 1) * 18 + (led - 1) * 3 + (color + 2)
+# (TODO: Remember why we add 2...)
+
 from sys import argv, exit
 
 bulbs = {}
@@ -25,13 +34,13 @@ for line in open(boxFile):
 		exit(1)
 	bulb = int(items[1])
 	rgb = bulbs[bulb]
-	for i in range(len(rgb)):
-		physical = (box - 1) * 18 + (boxLed - 1) * 3 + (i + 2)
-		print('%d\t%d' % (rgb[i], physical))
+	for color in range(len(rgb)):
+		physical = (box - 1) * 18 + (boxLed - 1) * 3 + (color + 2)
+		print('%d\t%d' % (rgb[color], physical))
 
-# Counter
-for i in range(2):
-	for j in range(7):
-		logical = i * 7 + j + 1
-		physical = i * 9 + j + 488
+# The two seven-segment displays that make up the counter
+for display in range(2):
+	for segment in range(7):
+		logical = display * 7 + segment + 101
+		physical = display * 9 + segment + 488
 		print('%d\t%d' % (logical, physical))
