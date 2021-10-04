3451bb0482741c9b93a234dd2b13f591fe5bed74
Ole Sivert Aarhaug <ole.sivert@gmail.com>
2021-10-04 18:47:25 +0200

    More sequences and started work on intro

--- generator/src/kotlin/darkness/generator/scripts/uka21/Snake.kt
+++ generator/src/kotlin/darkness/generator/scripts/uka21/Snake.kt
@@ -8,7 +8,7 @@ class Snake : BaseScript() {
     override suspend fun run() {
         super.run()
 
-        set(allBulbsGroup, uke_lilla)
+        rgbFade(allBulbsGroup, uke_lilla,10)
         skip(20)
 
         for (letter in letters) {
@@ -16,46 +16,45 @@ class Snake : BaseScript() {
             skip(5)
         }
 
-        set(allBulbsGroup, uke_lilla)
+        rgbFade(allBulbsGroup, uke_lilla, 10)
         skip(20)
-        var i = 0
-        for (bulb in allBulbsGroup) {
-            var bulb_nr1 = i - 4*1 
-            var bulb_nr2 = i - 4*2
-            var bulb_nr3 = i - 4*3
-            set(bulb, uke_gull)
-            if (bulb_nr1 > 0) {
+        var trail_length = 4
+        for (i in 0 until allBulbs.size+trail_length*3) {
+            var bulb_nr1 = i - trail_length*1 
+            var bulb_nr2 = i - trail_length*2
+            var bulb_nr3 = i - trail_length*3
+            if (i >= 0 && i < allBulbs.size) {
+                set(allBulbs[i], uke_gull)
+            }
+            if (bulb_nr1 >= 0 && bulb_nr1 < allBulbs.size) {
                 set(allBulbs[bulb_nr1], uke_blå)
             }
-            if (bulb_nr2 > 0) {
+            if (bulb_nr2 >= 0 && bulb_nr2 < allBulbs.size) {
                 set(allBulbs[bulb_nr2], uke_gull)
             }
-            if (bulb_nr3 > 0) {
+            if (bulb_nr3 >= 0 && bulb_nr3 < allBulbs.size) {
                 set(allBulbs[bulb_nr3], uke_blå)
-                set(allBulbs[0], uke_blå)
             }
             skip(1)
-            i = i + 1
         }
-
-        i = allBulbs.size
-        for (bulb in allBulbsGroup) {
-            var bulb_nr1 = i + 4*1 
-            var bulb_nr2 = i + 4*2
-            var bulb_nr3 = i + 4*3
-            set(bulb, uke_gull)
-            if (bulb_nr1 < allBulbs.size) {
-                set(allBulbs[bulb_nr1], uke_blå)
+        trail_length = 8
+       for (i in allBulbs.size+trail_length*3-1 downTo 0-trail_length*3-1 ) {
+            var bulb_nr1 = i + trail_length*1 
+            var bulb_nr2 = i + trail_length*2
+            var bulb_nr3 = i + trail_length*3
+            if (i >= 0 && i < allBulbs.size) {
+                set(allBulbs[i], uke_gull)
             }
-            if (bulb_nr2 < allBulbs.size) {
+            if (bulb_nr1 >= 0 && bulb_nr1 < allBulbs.size) {
+                set(allBulbs[bulb_nr1], uke_rød)
+            }
+            if (bulb_nr2 >= 0 && bulb_nr2 < allBulbs.size) {
                 set(allBulbs[bulb_nr2], uke_gull)
             }
-            if (bulb_nr3 < allBulbs.size) {
-                set(allBulbs[bulb_nr3], uke_blå)
-                //set(allBulbs[0], uke_blå)
+            if (bulb_nr3 >= 0 && bulb_nr3 < allBulbs.size) {
+                set(allBulbs[bulb_nr3], uke_lilla)
             }
             skip(1)
-            i = i - 1
         }
 
         rgbFade(allBulbsGroup, uke_gull,5)
@@ -67,6 +66,7 @@ class Snake : BaseScript() {
         rgbFade(allBulbsGroup, uke_blå,5)
         skip(10)
         rgbFade(allBulbsGroup, Color.black, 50)
+        
 
     }
 }
