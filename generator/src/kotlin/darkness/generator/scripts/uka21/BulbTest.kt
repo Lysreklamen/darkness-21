fd247d21132a9320e7435ef3e00494134c5a0145
Aasmund Eldhuset <aasmund.eldhuset@gmail.com>
2021-10-05 12:19:38 -0700

    Improved bulb test sequence.

--- generator/src/kotlin/darkness/generator/scripts/uka21/BulbTest.kt
+++ generator/src/kotlin/darkness/generator/scripts/uka21/BulbTest.kt
@@ -2,20 +2,21 @@ package darkness.generator.scripts.uka21
 
 import java.awt.Color
 
+// Meant to be run with --single-step
 class BulbTest : BaseScript() {
     override suspend fun run() {
         super.run()
         
         for(bulb in allBulbs){
             set(bulb, 255, 0, 0)
-            skip(2)
+            next()
             set(bulb, 0, 255, 0)
-            skip(2)
+            next()
             set(bulb, 0, 0, 255)
-            skip(2)
+            next()
+            set(bulb, 255, 255, 255)
+            next()
             set(bulb, 0, 0, 0)
-            skip(2)
         }
-
     }
 }
