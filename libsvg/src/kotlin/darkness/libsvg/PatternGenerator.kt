ce6dfa17dbb6d27b763359602ae9ec5b7946a375
Simon Krane <simonkrane11@gmail.com>
2023-02-02 20:39:36 +0100

    Started updating the SVG parser to the new sign format.

--- libsvg/src/kotlin/darkness/libsvg/PatternGenerator.kt
+++ libsvg/src/kotlin/darkness/libsvg/PatternGenerator.kt
@@ -9,9 +9,12 @@ class PatternGenerator(val parser: SVGParser) {
             val output = StringBuilder()
             val scale = 0.01f
 
-            output.append("OFFSET 0 0\n")
-            output.append("SCALE 1 1\n")
-            output.append("\n\n")
+            output.append("""
+                {
+                    "format_versin": "v0.1.0",
+                    "groups": [
+                """.trimIndent() + "\n"
+            )
 
             if (!parser.backgroundOutline.isEmpty) {
                 output.append("BACKGROUND (%.3f, %.3f, %.3f, %.3f)".format(parser.backgroundOutline.x * scale,
@@ -25,35 +28,65 @@ class PatternGenerator(val parser: SVGParser) {
                 output.append("\n\n")
             }
 
-            for (shape in parser.letters) {
-                output.append("ALU")
-                for (p in shape.outline) {
-                    output.append(" (%.3f, %.3f);".format(p.x / 100.0f, p.y / 100.0f))
-                }
-                for (hole in shape.holes) {
-                    output.append(" -;")
-                    for (p in hole) {
-                        output.append(" (%.3f, %.3f);".format(p.x / 100.0f, p.y / 100.0f))
+            for ((s, shape) in parser.letters.withIndex()) {
+                output.append("""        {
+            "pos": [0.0, 0.0],
+            "alu": {
+                "outline": [
+"""
+                )
+
+                printPoints(shape.outline, output, 20)
+                output.append("${" ".repeat(16)}],\n")
+                output.append("${" ".repeat(16)}\"holes\": [\n")
+
+                for ((i,holePoints) in shape.holes.withIndex()){
+                    output.append("${" ".repeat(20)}[\n")
+                    printPoints(holePoints, output, 24)
+                    output.append("${" ".repeat(20)}]")
+                    if( i != shape.holes.size-1){
+                        output.append(",")
                     }
+                    output.append("\n")
                 }
-                output.append("\n\n")
+                output.append("${" ".repeat(16)}]\n")
+                output.append("${" ".repeat(12)}},\n")
+                output.append("${" ".repeat(12)}\"bulbs\":[]\n")
+
+                output.append("${" ".repeat(8)}}")
+                if(s != parser.letters.size -1){
+                    output.append(",")
+                }
+                output.append("\n")
+
             }
+            output.append("]\n}")
 
             output.append("\n\n")
 
-            for ((bulbId, bulb) in parser.bulbs.entries) {
-                var rChan = 200 +bulbId
-                var gChan = 300 +bulbId
-                var bChan = 400 +bulbId
-                if (bulbId >= 100) {
-                    rChan = bulbId-100
-                    gChan = 50+bulbId-100
-                    bChan = 100+bulbId-100
-                }
-                output.append("%d (%.2f, %.2f) R %d %d %d\n".format(bulbId, bulb.x / 100.0f, bulb.y / 100.0f, rChan, gChan, bChan))
-            }
-            output.append("\n\n")
+//            for ((bulbId, bulb) in parser.bulbs.entries) {
+//                var rChan = 200 +bulbId
+//                var gChan = 300 +bulbId
+//                var bChan = 400 +bulbId
+//                if (bulbId >= 100) {
+//                    rChan = bulbId-100
+//                    gChan = 50+bulbId-100
+//                    bChan = 100+bulbId-100
+//                }
+//                output.append("%d (%.2f, %.2f) R %d %d %d\n".format(bulbId, bulb.x / 100.0f, bulb.y / 100.0f, rChan, gChan, bChan))
+//            }
+//            output.append("\n\n")
 
             return output.toString()
         }
+
+    private fun printPoints(points: List<SVGParser.Point>, output: StringBuilder, indentation: Int) {
+        for ((i, p) in points.withIndex()) {
+            output.append("${" ".repeat(indentation)}[${p.x}, ${-p.y}]")
+            if (i != points.size - 1) {
+                output.append(",")
+            }
+            output.append("\n")
+        }
+    }
 }
